33a7a84573a04900f2169648122ca0a0438e9364
Karivella James <karivella.james2022.com>
2023-07-12 11:39:18 +0000

    Notifications

---
+++ index.js
@@ -0,0 +1,41 @@
+const Notification = (props) => {
+  const { className, iconUrl, message } = props;
+  const containerClassName = `notification-container ${className}`;
+
+  return (
+    <div className={containerClassName}>
+      <img className="icon" src={iconUrl} />
+      <p className="message">{message}</p>
+    </div>
+  );
+};
+
+const element = (
+  <div className="bg-container">
+    <h1 className="main-heading">Notifications</h1>
+    <div className="notifications-list-container">
+      <Notification
+        className="primary-msg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+        message="Information Message"
+      />
+      <Notification
+        className="success-msg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+        message="Success Message"
+      />
+      <Notification
+        className="warning-msg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+        message="Warning Message"
+      />
+      <Notification
+        className="danger-msg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
+        message="Error Message"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
